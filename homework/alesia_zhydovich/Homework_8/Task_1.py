f4738e81edb96d55e05a5f1172ea02ea86ca2e50
a-sellika <alesiage@gmail.com>
2025-03-26 20:38:38 +0200

    Homework_8 done

---
+++ homework/alesia_zhydovich/Homework_8/Task_1.py
@@ -0,0 +1,18 @@
+import random
+
+
+salary = int(input('Укажи размер зарплаты: '))
+bonus = random.choice ([True, False])
+
+
+def amount(salary, bonus):
+    if bonus == True:
+        bonus_amount = random.randrange(500, 10000, 100)
+        salary += bonus_amount
+        print(f'Бонус активен! Добавлено: {bonus_amount}')
+    else:
+        print( 'Бонус не активен.')
+    print(f'Итоговая зарплата: ${salary}')
+
+
+amount(salary, bonus)
---
+++ homework/alesia_zhydovich/Homework_8/Task_2.py
@@ -0,0 +1,13 @@
+def print_fibonacci(index):
+    first = 0
+    second  = 1
+    count = 1
+    while count <= 100001:
+        if count in index:
+            print(f"{count}-е число Фибоначчи: {first}")
+        first, second = second, first + second
+        count += 1
+
+
+index = [1, 5, 200, 1000, 100000]
+print_fibonacci(index)
