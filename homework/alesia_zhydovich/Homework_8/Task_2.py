91b119578fc1ef252d20501caaddec43a61d1b3d
a-sellika <alesiage@gmail.com>
2025-03-27 20:14:56 +0200

    Homework_8 Task_2 has been reworked

--- homework/alesia_zhydovich/Homework_8/Task_2.py
+++ homework/alesia_zhydovich/Homework_8/Task_2.py
@@ -1,13 +1,18 @@
-def print_fibonacci(index):
+def fibonacci():
     first = 0
     second = 1
     count = 1
     while count <= 100001:
-        if count in index:
-            print(f"{count}-е число Фибоначчи: {first}")
+        yield first
         first, second = second, first + second
         count += 1
 
 
 index = [1, 5, 200, 1000, 100000]
-print_fibonacci(index)
+index_num = 1
+for num in fibonacci():
+    if index_num in index:
+        print(f"{index_num}-е число Фибоначчи: {num}")
+    index_num += 1
+    if index_num > max(index):
+        break
